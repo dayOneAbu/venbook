a0d48f0627ddd6d78b4e6d2220f042635c86fed7
DayOneAbu <abubker.init@gmail.com>
2026-02-15 18:33:47 +0300

    fixied adapterversion issue

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,15 +1,11 @@
 import { env } from "~/env";
 import { PrismaClient } from "../../generated/prisma";
-import { neonConfig } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
-import ws from "ws";
 
-// Must be the FIRST thing to run
-neonConfig.webSocketConstructor = ws;
-
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaNeon({ connectionString });
+const pool = new Pool({ connectionString: env.DATABASE_URL });
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
+const adapter = new PrismaNeon(pool as any);
 
 const createPrismaClient = () =>
   new PrismaClient({
